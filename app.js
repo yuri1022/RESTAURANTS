088be66c359468944ad2224049771de6b81efd9a
yuri1022 <miu10067@gmail.com>
2023-11-09 19:50:45 +0800

    feat:add listing page and stylesheets

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { engine } = require('express-handlebars')
+const restaurants = require('./public/jsons/restaurant.json').results
+
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
+
 app.use(express.static('public'))
 
 
@@ -14,7 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurants })
 })
 
 app.get('/restaurant/:id', (req, res) => {
