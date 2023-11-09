ea94b340b5525212c31016bb6f23e6939bbb7950
yuri1022 <miu10067@gmail.com>
2023-11-09 15:37:45 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for restaurant')
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('index')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`introduction of restaurants:${id}`)
 })
 
 app.listen(port, () => {
